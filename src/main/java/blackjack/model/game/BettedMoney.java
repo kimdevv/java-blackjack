bd07cc7d52cfb7414424d7eca04457b140f07c5d
kimdev <kim__dev@hufs.ac.kr>
2025-03-14 11:14:20 +0900

    refactor: 베팅 가능한 최소 금액 수정

--- src/main/java/blackjack/model/game/BettedMoney.java
+++ src/main/java/blackjack/model/game/BettedMoney.java
@@ -2,7 +2,7 @@ package blackjack.model.game;
 
 public class BettedMoney {
 
-    public static final int MINIMUM_AVAILABLE_MONEY = 0;
+    public static final int MINIMUM_AVAILABLE_MONEY = 10_000;
     public static final int MAXIMUM_AVAILABLE_MONEY = 1_000_000_000;
     private final int money;
 
@@ -13,7 +13,7 @@ public class BettedMoney {
 
     private void validate(final int money) {
         if (money < MINIMUM_AVAILABLE_MONEY) {
-            throw new IllegalArgumentException("0원 이상 베팅 가능합니다.");
+            throw new IllegalArgumentException(String.format("%d원 이상 베팅 가능합니다.", MINIMUM_AVAILABLE_MONEY));
         }
         if (money > MAXIMUM_AVAILABLE_MONEY) {
             throw new IllegalArgumentException("최대 10억 원까지 베팅 가능합니다.");
